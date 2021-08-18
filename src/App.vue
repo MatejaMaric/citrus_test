d8554cb57e95e106ffad770007f3070962df049f
Mateja <mail@matejamaric.com>
2021-08-18 14:40:45 +0200

    Display selected user's name.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <Users />
+  <div v-if="selectedUser">
+    <div>{{ selectedUser.name.split(" ")[0].toUpperCase() }}'S TODOS</div>
+  </div>
 </template>
 
 <script>
 import Users from "@/components/Users";
+import { computed } from "vue";
+import { useStore } from "vuex";
 
 export default {
   name: "App",
   components: { Users },
-  setup() {},
+  setup() {
+    const store = useStore();
+
+    const selectedUser = computed(() => store.getters.getSelectedUser);
+
+    return { selectedUser };
+  },
 };
 </script>
 
